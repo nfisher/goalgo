2be55cfe17917e05db5606649000e4f9e24187bc
Nathan Fisher <nfisher@junctionbox.ca>
2020-12-18 20:01:11 -0500

    Fix spelling error

--- graph/errors/errors.go
+++ graph/errors/errors.go
@@ -5,7 +5,7 @@ import "errors"
 var (
 	// ErrCannotAddVertices is emitted when an invalid edge is specified in the creation of a new vertex.
 	ErrCannotAddVertices = errors.New("graph: cannot add vertices with invalid edges")
-	// ErrCannotAddEdge is emitted when one or more of the vertices in an edge are invalid/non-existant.
+	// ErrCannotAddEdge is emitted when one or more of the vertices in an edge are invalid/non-existent.
 	ErrCannotAddEdge = errors.New("graph: cannot add edge with invalid vertices")
 	// ErrVertexNotFound is emitted when a vertex does not exist and therefore has no edge set.
 	ErrVertexNotFound = errors.New("graph: vertex not found")
